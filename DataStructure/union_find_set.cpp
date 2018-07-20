66a0914cc18be29df305f10ef56247bc9f839c5d
XanderXu1998 <xanderxu1998@gmail.com>
2018-07-20 12:24:01 +0800

    Update union_find_set.cpp

--- DataStructure/union_find_set.cpp
+++ DataStructure/union_find_set.cpp
@@ -1,13 +1,12 @@
-int father[20001];
-int m, n, i, x, y, q;
+int fa[20001];
 int find(int x) //用于查找一个元素的所在集合即根节点
 {
-    if (father[x] != x) father[x] = find(father[x]); //路径压缩，递归将路径上的元素父亲指针都指向根节点
+    if (fa[x] != x) fa[x] = find(fa[x]); //路径压缩，递归将路径上的元素父亲指针都指向根节点
     return father[x];
 }
-void u(int r1, int r2) //合并两个集合
+void merge(int r1, int r2) //合并两个集合，r2合并到r1
 {
     r1 = find(r1);
     r2 = find(r2);
-    if (r1 != r2) father[r2] = r1;
+    if (r1 != r2) fa[r2] = r1;
 }
